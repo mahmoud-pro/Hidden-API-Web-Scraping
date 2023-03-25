859ffbbb2dbf274906ffa1f95727f4c57da2de23
mahmoud-pro <56353916+mahmoud-pro@users.noreply.github.com>
2023-03-25 12:35:31 +0200

    update location to become dynamic

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 import requests as r
 import pandas as pd
+from pgeocode import Nominatim
 
 
-def get_job(lat=None, lng=None, results=24):
-    if lat is None or lng is None:
+def get_job(lat=None, lng=None, postal_code=None, results=24):
+    if (lat is None or lng is None) and postal_code is None:
         raise ValueError("Both lat and lng must be provided")
+
+    if postal_code is not None:
+        nomi = Nominatim("ca")
+        geo = nomi.query_postal_code(postal_code)
+        lat = geo.latitude
+        lng = geo.longitude
 
     url = f"https://api.higherme.com/classic/jobs?page=1&includes=location,location.company&limit={results}&filters^[" \
           f"brand.id^]=58bd9e7f472bd&filters^[lat^]={lat}&filters^[lng^]={lng}&filters^[" \
@@ -24,25 +31,24 @@ def get_job(lat=None, lng=None, results=24):
       'TE': 'trailers'
     }
 
-    response = r.request("GET", url, headers=headers)
+    response = r.request("GET", url, headers=headers).json()
 
-    return response.json()
+    df = pd.DataFrame(
+        data=[req.get("attributes") for req in response.get('data')],
+        columns=["title", "full_time", "part_time", "requirements", "distance"])
+
+    df.to_csv("jobs.csv", index=False)
+    df.to_json("jobs.json")
+
+    return df
 
 
 # title
 # full_time/part_time
 # requirements
 # distance
+get_job(postal_code="m2m")
 
-response_data = get_job(43.6532, -79.3832)
-# print(response_data.get("data"))
 
-df = pd.DataFrame(
-    data=[r.get("attributes") for r in response_data.get('data')],
-    columns=["title", "full_time", "part_time", "requirements", "distance"])
-
-df.to_csv("jobs.csv", index=False)
-df.to_json("jobs.json")
-# print(df)
 
 
